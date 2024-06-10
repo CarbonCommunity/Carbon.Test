2f8027aaf3271d0390af8ec7b9ec85f14415fba1
raul <raulssorban@gmail.com>
2024-06-10 18:18:40 +0200

    Initial integrations

---
+++ src/Integrations.cs
@@ -0,0 +1,99 @@
+﻿/*
+ *
+ * Copyright (c) 2024 Carbon Community
+ * All rights reserved.
+ *
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using UnityEngine;
+using ILogger = API.Logger.ILogger;
+
+namespace Carbon.Test;
+
+public static class Integrations
+{
+	public static ILogger Logger { get; set; }
+	public static Stopwatch Stopwatch { get; } = new();
+	public static Queue<TestBed> TestBeds { get; } = new();
+
+	public static TestBed GetTestBed(string context, Type type)
+	{
+		var bed = new TestBed(context);
+
+		foreach (var method in type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+		{
+			var attribute = method.GetCustomAttribute<Test>();
+
+			if (attribute == null)
+			{
+				continue;
+			}
+
+			bed.AddTest(type, method, attribute);
+		}
+
+		return bed;
+	}
+	public static IEnumerator Run(float delay)
+	{
+		while (TestBeds.Count != 0)
+		{
+			var bed = TestBeds.Dequeue();
+
+			Logger.Console($"Initialized Test bed - {bed.Context}");
+			foreach (var test in bed)
+			{
+				Stopwatch.Restart();
+
+				test.Run();
+
+				while (test.IsRunning)
+				{
+					test.SetDuration(Stopwatch.Elapsed);
+					test.RunCheck();
+					yield return null;
+				}
+
+				if (test.ShouldCancel())
+				{
+					break;
+				}
+
+				if (delay > 0)
+				{
+					yield return CoroutineEx.waitForSecondsRealtime(delay);
+				}
+				else
+				{
+					yield return null;
+				}
+			}
+
+			Logger.Console($"Completed {bed.Count:n0} tests - {bed.Context}");
+
+			yield return null;
+		}
+	}
+
+	public class TestBed : List<Test>
+	{
+		public string Context;
+
+		public TestBed() { }
+		public TestBed(string context)
+		{
+			Context = context;
+		}
+
+		public void AddTest(Type type, MethodInfo method, Test test)
+		{
+			test.Setup(type, method);
+			Add(test);
+		}
+	}
+}
---
+++ src/Test.cs
@@ -0,0 +1,158 @@
+﻿/*
+ *
+ * Copyright (c) 2024 Carbon Community
+ * All rights reserved.
+ *
+ */
+
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using API.Logger;
+using API.Plugins;
+
+namespace Carbon.Test;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class Test : Attribute
+{
+	public string Name;
+	public float Timeout = -1;
+	public bool CancelOnFail = false;
+
+	public enum StatusTypes
+	{
+		None,
+		Running,
+		Success,
+		Failure,
+		Fatal,
+		Timeout
+	}
+
+	internal Type _type;
+	internal MethodInfo _method;
+	internal object _target;
+	internal StatusTypes _statusType;
+
+	internal TimeSpan _duration;
+
+	internal static object[] _args = new object[1];
+
+	public bool IsRunning => Status == StatusTypes.Running;
+	public StatusTypes Status => _statusType;
+
+	public void Setup(Type type, MethodInfo info)
+	{
+		_type = type;
+		_method = info;
+	}
+	public void SetDuration(TimeSpan span)
+	{
+		_duration = span;
+	}
+	public void SetStatus(StatusTypes status)
+	{
+		_statusType = status;
+	}
+
+	public void Run()
+	{
+		SetStatus(StatusTypes.Running);
+
+		_args[0] = this;
+		_method.Invoke(_target, _args);
+	}
+	public void RunCheck()
+	{
+		if (Timeout <= 0)
+		{
+			return;
+		}
+
+		if (_duration.TotalSeconds >= Timeout)
+		{
+			OnTimeout();
+		}
+	}
+
+	public void OnSucceed()
+	{
+		SetStatus(StatusTypes.Success);
+		Log($"Success");
+	}
+	public void OnTimeout()
+	{
+		SetStatus(StatusTypes.Timeout);
+		Warn($"Timed out >= {Timeout * 1000:00}ms");
+	}
+	public void Fail(string reason, Exception ex = null)
+	{
+		SetStatus(StatusTypes.Failure);
+		Error($"Fail - {reason}", ex);
+	}
+	public void FatalFail(string reason, Exception ex = null)
+	{
+		SetStatus(StatusTypes.Fatal);
+		Error($"Fatal - {reason}", ex);
+	}
+
+	public bool ShouldCancel() => Status == StatusTypes.Fatal || (CancelOnFail && Status != StatusTypes.Success);
+
+	public virtual string ToPrettyString() => $"{Name}|{_method.Name}|{_duration.TotalMilliseconds:00}ms";
+
+	public void Log(string message)
+	{
+		Integrations.Logger.Console($"{ToPrettyString()}  {message}");
+	}
+	public void Warn(string message)
+	{
+		Integrations.Logger.Console($"{ToPrettyString()}  {message}", Severity.Warning);
+	}
+	public void Error(string message, Exception exception)
+	{
+		Integrations.Logger.Console($"{ToPrettyString()}  {message}", Severity.Error, exception);
+	}
+	public void Fatal(string message, Exception exception)
+	{
+		Error(message, exception);
+		SetStatus(StatusTypes.Fatal);
+	}
+
+	[AttributeUsage(AttributeTargets.Method)]
+	public class Assert : Test
+	{
+		public override string ToPrettyString() => base.ToPrettyString() + "|Assert";
+
+		public static bool IsTrue(bool condition)
+		{
+			return condition;
+		}
+		public static bool IsFalse(bool condition)
+		{
+			return IsTrue(condition);
+		}
+		public static bool IsNull(object value)
+		{
+			return value == null;
+		}
+		public static bool IsNotNull(object value)
+		{
+			return !IsNull(value);
+		}
+	}
+
+	[AttributeUsage(AttributeTargets.Method)]
+	public class WaitUntil : Test
+	{
+		internal bool Finalized;
+
+		public override string ToPrettyString() => base.ToPrettyString() + "|WaitUntil";
+
+		public void OnDone()
+		{
+			Finalized = true;
+			OnSucceed();
+		}
+	}
+}
